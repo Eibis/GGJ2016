acb280d7bf2385b274c3a47d25dbeeaa41008952
Davide De Gennaro <nerevar88@gmail.com>
2016-01-29 23:20:34 +0100

    Telecamera 2D & collider continuo

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+    public GameObject Demon;
+	
+	// Update is called once per frame
+	void FixedUpdate()
+    {
+        this.transform.position = new Vector3(Demon.transform.position.x, this.transform.position.y, this.transform.position.z);
+	}
+}
