c17934e2a68ec54bec27a8ceda68e3f496a0d645
Dedalus IT Solutions Provider <Dedalus IT Solutions Provider>
2016-01-29 22:25:12 +0100

    movimento e piano

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public Camera camera3d;
+    public Camera camera2d;
+
+    public static GameManager Istance { get { return _this; } }
+    private static GameManager _this;
+
+	// Use this for initialization
+	void Start () {
+        _this = this;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Pickable.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pickable : MonoBehaviour {
+
+    private Vector3 initial_position;
+    bool moved = false; 
+
+	// Use this for initialization
+	void Start () {
+
+        initial_position = transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (Input.GetMouseButtonDown(0))
+        { // if left button pressed...
+            Ray ray = GameManager.Istance.camera3d.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                moved = !moved;
+                Debug.Log("clicked");
+            }
+        }
+
+    }
+
+    
+}
