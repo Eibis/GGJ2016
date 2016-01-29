30014b2aa0ad25af0be40e3217aec7cac4f9d4e5
Matteo Bevan <matteo.bevan@gmail.com>
2016-01-30 00:59:35 +0100

    modelli e ui

---
+++ Assets/Scripts/FadeText.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeText : MonoBehaviour
+{
+    public static FadeText Instance {
+        get
+        {
+            return _this;
+        }
+    }
+
+    private static FadeText _this;
+
+	void Start ()
+    {
+        _this = this;
+	}
+	
+	void Update ()
+    {
+	
+	}
+
+    public static void Fade()
+    {
+
+    }
+}
