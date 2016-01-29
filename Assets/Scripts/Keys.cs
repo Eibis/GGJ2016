22b0bb91ed30ee8ee47dffe367e7e15fb630b65a
Dorus <gianmarco.doro@gmail.com>
2016-01-29 22:35:35 +0100

    Added 2D stuff

---
+++ Assets/Scripts/Keys.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Keys : MonoBehaviour {
+
+	int jumpSpeed = 100;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("right")) {
+			Vector3 position = this.transform.position;
+			position.x++;
+			this.transform.position = position;
+		}
+		if (Input.GetKey ("left")) {
+			Vector3 position = this.transform.position;
+			position.x--;
+			this.transform.position = position;
+		}
+	}
+
+	void OnCollisionStay2D(Collision2D coll)
+	{
+		if (coll.gameObject.tag == "Ground" && (Input.GetKey("up"))) {
+			GetComponent<Rigidbody2D>().AddForce (Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+		}
+	}
+}
