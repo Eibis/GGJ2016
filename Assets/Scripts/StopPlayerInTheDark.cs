a5079683688548cc2fa151262d43a279e68f5763
Dorus <gianmarco.doro@gmail.com>
2016-01-30 22:24:21 +0100

    Working on first level

---
+++ Assets/Scripts/StopPlayerInTheDark.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StopPlayerInTheDark : MonoBehaviour {
+
+	public GameObject Demon;
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		Debug.Log ("Culo");
+		if(coll.gameObject == Demon )
+		{
+			Demon.GetComponent<Keys>().enabled = false;
+		}
+	}
+}
