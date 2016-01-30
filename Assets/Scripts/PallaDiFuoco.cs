3bd218e994cf387aab9ff2a644904e36fdf89d5a
Dedalus IT Solutions Provider <Dedalus IT Solutions Provider>
2016-01-30 19:24:13 +0100

    Inizio gestione palla di fuoco

---
+++ Assets/Scripts/PallaDiFuoco.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PallaDiFuoco : MonoBehaviour {
+
+	// Use this for initialization
+    float thrust = 0.4f;
+    
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        Debug.Log("start");
+        rb = GetComponent<Rigidbody2D>();
+        transform.position = new Vector3(0f, 10f, 0f);
+
+        if(gameObject.GetComponent<Keys>().destra)
+
+            rb.AddForce(transform.right*500);
+        else
+            rb.AddForce(transform.right * -500);
+
+    }
+    void Update()
+    {
+
+    }
+}
