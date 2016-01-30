bb3398472c4523900f6c4afc52967fd6a8b1b287
Dedalus IT Solutions Provider <Dedalus IT Solutions Provider>
2016-01-30 09:35:39 +0100

    sistemato salto

--- Assets/Scripts/Keys.cs
+++ Assets/Scripts/Keys.cs
@@ -30,7 +30,7 @@ public class Keys : MonoBehaviour {
     void FixedUpdate ()
     {
 
-        isGrounded = old_y>=this.transform.position.y && Physics2D.Linecast(this.transform.position, transform.position - new Vector3(0, 0.5f, 0), 1 << LayerMask.NameToLayer("Ground"));
+        isGrounded = old_y>=this.transform.position.y && Physics2D.Linecast(this.transform.position, transform.position - new Vector3(0, 0.2f, 0), 1 << LayerMask.NameToLayer("Ground"));
 		if (Input.GetKey ("right") || Input.GetAxis("Horizontal") > 0) {
 			transform.position += Vector3.right * lateral_speed * Time.deltaTime;
 		}
@@ -42,7 +42,7 @@ public class Keys : MonoBehaviour {
 			this.transform.position = new Vector3(-wrld.x + half_sz,
 												  this.transform.position.y,
 											      this.transform.position.z);
-        if (isGrounded && (Input.GetKey("up") || Input.GetKey(KeyCode.JoystickButton0)))
+        if (isGrounded && (Input.GetKey("up") || Input.GetKey(KeyCode.JoystickButton0) || Input.GetKey(KeyCode.W)))
         {
             GetComponent<Rigidbody2D>().AddForce(Vector2.up * jump_speed, ForceMode2D.Impulse);
         }
