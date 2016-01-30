847291866e43d5b322f9d573f48b2ac82fe5eecc
Dorus <gianmarco.doro@gmail.com>
2016-01-30 19:37:22 +0100

    added second level

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -5,12 +5,10 @@ public class CameraMovement : MonoBehaviour {
     public GameObject Demon;
     public GameObject bg;
 	public GameObject Foreground_liv1;
-    float bg_lateral_speed = 0.1f;
 
     // Update is called once per frame
     void LateUpdate()
     {
-		Vector3 startingPosition = transform.position;
 		float new_left_bounded_x_position = Mathf.Max(Demon.transform.position.x, Foreground_liv1.GetComponent<Renderer> ().bounds.min.x + this.GetComponent<Camera> ().orthographicSize * Screen.width / Screen.height);
 		float new_x_position = Mathf.Min (new_left_bounded_x_position, Foreground_liv1.GetComponent<Renderer> ().bounds.max.x - this.GetComponent<Camera> ().orthographicSize * Screen.width / Screen.height);
 		this.transform.position = new Vector3(new_x_position,
